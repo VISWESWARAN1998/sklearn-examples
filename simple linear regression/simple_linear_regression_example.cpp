858c98fd47c1e2d055fa5f0defafb4073de71e1a
unknown <visweswaran.nagasivam98@gmail.com>
2020-04-11 14:05:33 +0530

    Simple linear regression :heart:

---
+++ simple linear regression/simple_linear_regression_example.cpp
@@ -0,0 +1,12 @@
+// SWAMI KARUPPASWAMI THUNNAI
+
+#include "lsr.h"
+
+int main()
+{
+	// X, y, print_debug messages
+	simple_linear_regression slr({2, 3, 5, 7, 9}, {4, 5, 7, 10, 15}, DEBUG);
+	slr.fit();
+	std::cout << slr.predict(8);
+	slr.save_model("model.txt");
+}
